ecc08abb1138ba2a40d087a2c7fbd59380118165
SantiagoZarate <santiago2001zarate@gmail.com>
2024-06-30 15:33:41 -0300

    feat: añade informacion false y constrains

--- stored-procedures/bedelia/ddl.sql
+++ stored-procedures/bedelia/ddl.sql
@@ -1,7 +1,30 @@
+\c postgres
+
+DROP DATABASE IF EXISTS bedelia;
+CREATE DATABASE bedelia;
+
+\c bedelia
+
 CREATE TABLE alumne(
   legajo INT,
   nombre VARCHAR(100),
   apellido VARCHAR(100)
 );
 
+CREATE TABLE materia(
+  codigo INT,
+  nombre VARCHAR(100)
+);
+
+CREATE TABLE cursa(
+  legajo_alumno INT,
+  codigo_materia INT
+);
+
 ALTER TABLE alumne ADD PRIMARY KEY (legajo);
+
+ALTER TABLE materia ADD PRIMARY KEY (codigo);
+
+ALTER TABLE cursa ADD CONSTRAINT legajo_alumno_fk FOREIGN KEY (legajo_alumno) REFERENCES alumne (legajo);
+ALTER TABLE cursa ADD CONSTRAINT codigo_materia_fk FOREIGN KEY (codigo_materia) REFERENCES materia (codigo);
+ALTER TABLE cursa ADD PRIMARY KEY (legajo_alumno, codigo_materia);
--- stored-procedures/bedelia/seed.sql
+++ stored-procedures/bedelia/seed.sql
@@ -3,3 +3,25 @@ INSERT INTO alumne (legajo, nombre, apellido) VALUES
 (2, 'Lionel', 'Messi'),
 (3, 'Kendrick', 'Lamar'),
 (4, 'Peter', 'Griffin');
+
+INSERT INTO materia (codigo, nombre) VALUES
+(1, 'Introducción a la programación'),
+(2, 'Sistemas Operativos y Redes'),
+(3, 'Programación 1'),
+(4, 'Programación 2'),
+(5, 'Programación 3'),
+(6, 'Problemas Socio-Económicos Contemporaneos');
+
+INSERT INTO cursa (legajo_alumno, codigo_materia) VALUES
+(1, 2), /*Santiago Zarate */
+(1, 5),
+(2, 6), /*Lionel Messi */
+(3, 3), /*Kendrick Lamar */
+(3, 4),
+(3, 5),
+(4, 1), /*Peter Griffin*/
+(4, 2),
+(4, 3),
+(4, 4),
+(4, 5),
+(4, 6);
