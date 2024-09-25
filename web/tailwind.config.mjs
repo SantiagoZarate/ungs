ebc52d8ae33cf094db6166283001d91d7eed6d27
SantiagoZarate <santiago2001zarate@gmail.com>
2024-09-25 01:47:00 -0300

    style: add basic text stylings

--- web/tailwind.config.mjs
+++ web/tailwind.config.mjs
@@ -1,8 +1,21 @@
+import tailwindTypo from "@tailwindcss/typography";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
+  darkMode: ["class"],
+  theme: {
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",
+      },
+      fontFamily: {
+        geist: ["Geist", "sans-serif"],
+      },
+      screens: {
+        tablet: "805px",
+      },
+    },
+  },
+  plugins: [tailwindTypo],
+};
